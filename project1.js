861fb93aa46714c42e5328ffe42582bf6be8d53b
mtaghdiri <mtaghdiri72@gmail.com>
2023-02-23 15:50:25 +0330

    add style

--- project1.js
+++ project1.js
@@ -1,56 +1,52 @@
-var time = 00;
+var time = 0;
 var start = false;
-var check=true;
+var check = true;
 var c;
-var score = 0 ;
+var score = 0;
+var point;
 
 function startClicked() {
-    if (check){
+    if (check) {
         nextChar();
         check = false;
     }
-    start = !start; 
-    if (!start){
-        time = 0;
+    start = !start;
+    if (!start) {
+        time = 00;
         document.getElementById("start").innerHTML = "start";
-    } else{
+    } else {
         document.getElementById("start").innerHTML = "reset";
     }
 }
-setInterval(function (){
-if (start)
-    time = time+1;
-    document.getElementById("tens").innerHTML = time % 100;
-    document.getElementById("seconds").innerHTML = Math.floor(time / 100);
+setInterval(function () {
+    if (start)
+        time = time + 1;
+    point = Math.floor(time / 100) + ":" + time % 100
+        document.getElementById("time").innerHTML = point;
 }, 10);
 
- function nextChar() {
-   var index = Math.floor(Math.random() * 26);
-   var list = 'abcdefghijklmnopqrstuvwxyz'
-   c = list[index]
-   document.getElementById("letter").innerHTML = c ;
- }
+function nextChar() {
+    var index = Math.floor(Math.random() * 26);
+    var list = 'abcdefghijklmnopqrstuvwxyz'
+    c = list[index]
+    document.getElementById("letter").innerHTML = c;
+}
 
- document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', (event) => {
     var name = event.key;
     var random = document.getElementById('letter');
-    if(start){
-        if (random.innerHTML == name){
-            score = score +1
+    if (start) {
+        if (random.innerHTML == name) {
+            score = score + 1
             if (score == 10) {
                 start = false;
-                alert(time);
+                alert(point);
             }
 
             nextChar();
-            random.style.color="black";
-        }else{
-            random.style.color="red";
+            random.style.color = "black";
+        } else {
+            random.style.color = "red";
         }
     }
-  }, false);
-
-  function score () {
-    
-
-  }
+}, false);
